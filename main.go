ce6e32d5aa7f72cf774e7027c7bdff45517ffc96
Kinse <22683778+sofoxe1@users.noreply.github.com>
2024-07-21 11:56:24 +0200

    inital commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"image"
+	"image/color"
+
+	"github.com/llgcode/draw2d/draw2dimg"
+)
+
+func main() {
+	// Initialize the graphic context on an RGBA image
+	dest := image.NewRGBA(image.Rect(0, 0, 297, 210.0))
+	gc := draw2dimg.NewGraphicContext(dest)
+
+	// Set some properties
+	gc.SetFillColor(color.RGBA{0x44, 0xff, 0x44, 0xff})
+	gc.SetStrokeColor(color.RGBA{0x44, 0x44, 0x44, 0xff})
+	gc.SetLineWidth(5)
+
+	// Draw a closed shape
+	gc.MoveTo(10, 10) // should always be called first for a new path
+	gc.LineTo(100, 50)
+	gc.QuadCurveTo(100, 10, 10, 10)
+	gc.Close()
+	gc.FillStroke()
+
+	// Save to file
+	draw2dimg.SaveToPngFile("hello.png", dest)
+}
