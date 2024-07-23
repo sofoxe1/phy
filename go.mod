4e8d867548569c553e6f34ca293f53da2d4e6c60
Kinse <22683778+sofoxe1@users.noreply.github.com>
2024-07-23 02:29:31 +0200

    rectangle falls
    is it optimized/spaghetti free/fast (enough)?: no

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module phy
 
 go 1.21.12
-
-require github.com/go-playground/colors v1.3.1 // indirect
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"html/template"
 	"log"
-	"math/rand"
+	"math"
 	"net/http"
 	"os"
 	"os/exec"
@@ -19,20 +19,63 @@ type User struct {
     Name       string
     Occupation string
 }
+type object struct{
+	size float64
+	x float64
+	y float64
+	x_speed float64
+	y_speed float64
+	x_acceleration float64
+	y_acceleration float64
+	enabled *bool
+}
+var G = 9.8
+var time_step=0.22
+func (obj *object) update(){
+	fmt.Println(obj.y)
+obj.x=obj.x+obj.x_speed*time_step+(obj.x_acceleration*math.Pow(time_step,2))/2
+obj.y=obj.y-obj.y_speed*time_step+(obj.y_acceleration*math.Pow(time_step,2))/2
+obj.x_speed=obj.x_speed+obj.x_acceleration*time_step
+obj.y_speed=obj.y_speed+obj.y_acceleration*time_step
+}
+func (obj object) draw (frame *[512][512]uint8 ) (){
+	// fmt.Println(obj.x)
+	for  c_x := int(obj.x); c_x<=int(obj.x)+int(obj.size); c_x++{
+		for c_y:=int(obj.y); c_y<=int(obj.y)+int(obj.size); c_y++{
+			frame[c_y][c_x]=200
+			
+		}
+	} 
+}
 
-func get_data() ([]byte,error){
-	c:= [512][512]uint8{}
-	for i := 0; i < len(c); i ++{
-		for ii:= 0; ii<len(c[i]); ii++{
-			c[i][ii]=uint8(rand.Uint32()) //unit32 -> unit 8, u can do that, what????
+func step(){
+	for _,obj := range objects{
+		if *obj.enabled{
+		c=[512][512]uint8{}
+		obj.update()
+		obj.draw(&c)
 		}
 	}
+}
+var objects = make([]*object,0)
+var c = [512][512]uint8{}
+func get_data() ([]byte,error){
+	step()
 	jsonData, err := json.Marshal(c)
 	return jsonData,err
 }
+
+func (obj object) deafault() *object{
+	fmt.Println(obj.x)
+	if obj.size==0{obj.size=16}
+	if obj.enabled==nil{
+		obj.enabled=new(bool)
+		*obj.enabled=true
+	}
+	return &obj
+}
 func render() error{
 	j,err:=get_data()
-	// fmt.Println(j)
 	err2 := os.WriteFile("/tmp/goipc.json",j,0644)
 	if err !=nil{
 		log.Fatal(err)
@@ -61,6 +104,8 @@ func lock(w http.ResponseWriter, r *http.Request) {
 }
 func handleRequest(w http.ResponseWriter, r *http.Request) {
 	render()
+
+	
 	p := data{"meow:3"}
     t, err := template.ParseFiles("static/main.tmpl")
 	if err != nil{
@@ -72,6 +117,7 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 
 
 func main() {
+	objects = append(objects,object{x: 0, y:400,y_acceleration: G}.deafault())
 	
 	http.HandleFunc("/",handleRequest)
 	fs := http.FileServer(http.Dir("./dynamic"))
